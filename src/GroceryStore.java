4df76106ec8723d065c9e50488d5b22850b9052d
BT_3W2_05 <vincentl105@nycstudents.net>
2023-02-03 09:05:32 -0500

    Grocery Store

---
+++ src/GroceryStore.java
@@ -0,0 +1,61 @@
+import java.util.ArrayList;
+
+public class GroceryStore
+{
+    /** An array of products normally stocked at the grocery store
+     *  Guaranteed not to be null and to contain only non-null entries
+     */
+    private Product[] productsStocked;
+
+    public GroceryStore(Product[] initialInventory)
+    {
+        productsStocked = initialInventory;
+    }
+
+    /** Returns an ArrayList of indices for Product elements in the
+     *  productsStocked array that need to be reordered, as described in part (a)
+     *  Precondition: min > 0 */
+    public ArrayList<Integer> getReorderList(int min)
+    {
+        ArrayList<Integer> order = new ArrayList<Integer>();
+        for (int i = 0; i < productsStocked.length; i ++) {
+            if (productsStocked[i].getQuantity() <= min) {
+                order.add(i);
+            }
+        }
+        return order;
+    }
+
+    /** Returns true if all products named in shoppingList are available for purchase
+     *  and returns false otherwise, as described in part (b)
+     *  Precondition: The products named in shoppingList are found exactly once
+     *  in the productsStocked array.
+     */
+    public boolean checkAvailability(ArrayList<String> shoppingList)
+    {
+        for (String item : shoppingList) {
+            for (Product product : productsStocked) {
+                if (product.getName().equals(item)){
+                    if(product.getQuantity() == 0) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    /** Returns an ArrayList containing all Products from productStocked that
+     *  have a weight that exceeds the “weight” value provided in the parameter
+     */
+    public ArrayList<Product> oversized(double weight)
+    {
+        ArrayList<Product> newList = new ArrayList<Product>();
+        for (Product items : productsStocked) {
+            if (items.getWeight() > weight) {
+                newList.add(items);
+            }
+        }
+        return newList;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main (String [] args) {
+        Product p0 = new Product("cereal", 5, 1.75);
+        Product p1 = new Product("milk", 10, 2.3);
+        Product p2 = new Product("bread", 1, 0.75);
+        Product p3 = new Product("frozen pizza", 4, 1.3);
+        Product p4 = new Product("ice cream", 0, 2.1);
+        Product p5 = new Product("spaghetti", 6, 0.3);
+        Product p6 = new Product("muffins", 0, 0.25);
+        Product p7 = new Product("apples", 3, 3.75);
+    }
+}
